f382fadcb631109a86135edd200c6066fe3dcfcc
muvirag <a.malatinszki@gmail.com>
2019-08-13 15:38:36 +0200

    Create the first component, App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello there</h1>
+        <p>I am the first component</p>
+      </>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import { register } from './serviceWorker';
 
-ReactDOM.render(<h1>Hello there</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 register();
