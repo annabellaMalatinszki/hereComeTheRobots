3b4b663cfbcaad7004cc7770d1a7066d66f8a5a9
muvirag <a.malatinszki@gmail.com>
2019-08-13 19:14:15 +0200

    Create and assign a store to the app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
 import App from './containers/App';
 import { register } from './serviceWorker';
+import { searchRobots } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers(searchRobots);
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 register();
