0d88c76ade9e5122347bacedaba5aa44d8308665
muvirag <a.malatinszki@gmail.com>
2019-08-13 15:34:00 +0200

    Create index.js with sample h1 tag

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { register } from './serviceWorker';
+
+ReactDOM.render(<h1>Hello there</h1>, document.getElementById('root'));
+register();
