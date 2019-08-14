b12a593376248b627315c071a2ec6ab5932a4d9b
muvirag <a.malatinszki@gmail.com>
2019-08-14 10:49:23 +0200

    Set animation for cards

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc dib br2 pa3 ma2 bg-light-green">
+    <div className="tc dib br2 pa3 ma2 bg-light-green grow bw2 shadow-5">
       <img src={`https://robohash.org/${id}`} alt="photo" />
       <div>
         <h2>{name}</h2>
