ae62663cd2c5e9c84719530f75be2106862ec982
muvirag <a.malatinszki@gmail.com>
2019-08-21 12:06:23 +0200

    Log state to console using redux logger

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import 'tachyons';
 
 import App from './containers/App';
 import { register } from './serviceWorker';
 import { searchRobots, requestRobots } from './reducers';
 
+const logger = createLogger();
+
 const rootReducer = combineReducers({ searchRobots, requestRobots });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
