a6c82b16e2c61d19b7dcae5dbbd5d181988ce2f4
muvirag <a.malatinszki@gmail.com>
2019-08-21 10:52:09 +0200

    Fix missing curly brackets in index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './containers/App';
 import { register } from './serviceWorker';
 import { searchRobots, requestRobots } from './reducers';
 
-const rootReducer = combineReducers(searchRobots, requestRobots);
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(rootReducer);
 
 ReactDOM.render(
