d686d7827b4fe209a033c9beb7099da8e79838d3
muvirag <a.malatinszki@gmail.com>
2019-08-21 12:32:25 +0200

    Warning fix: replace alt attribute with empty string in Card image

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className="tc dib br2 pa3 ma2 bg-light-green grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}`} alt="photo" />
+      <img src={`https://robohash.org/${id}`} alt="" />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
