95f6fc2694a462b5f666815993f318acb7523ab8
muvirag <a.malatinszki@gmail.com>
2019-08-21 11:01:05 +0200

    Add padding to searchBox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBox = ({ searchChange }) => {
   return (
-    <div>
+    <div className="pa2">
       <input
         type="search"
         placeholder="Search Robots"
