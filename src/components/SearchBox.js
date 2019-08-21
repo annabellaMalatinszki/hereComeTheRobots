324e84c5ddc50d4970a3bf65f4d36cdd4c5af0e5
muvirag <a.malatinszki@gmail.com>
2019-08-21 11:01:53 +0200

    Add padding to searchBox input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
+        className="pa3 ba"
         type="search"
         placeholder="Search Robots"
         onChange={searchChange}
